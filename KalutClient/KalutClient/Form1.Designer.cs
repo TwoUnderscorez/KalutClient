ad821ac952ec22b0b78ba2e7253fa03b8cda2e51
Ron Yutkin <ron.yutkin@windowslive.com>
2017-05-07 22:43:28 +0300

    welcome screen gui v01

--- KalutClient/KalutClient/Form1.Designer.cs
+++ KalutClient/KalutClient/Form1.Designer.cs
@@ -28,12 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.metroTile4 = new MetroFramework.Controls.MetroTile();
+            this.metroTile2 = new MetroFramework.Controls.MetroTile();
+            this.metroTile1 = new MetroFramework.Controls.MetroTile();
+            this.SuspendLayout();
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(23, 60);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(68, 19);
+            this.metroLabel1.TabIndex = 1;
+            this.metroLabel1.Text = "I want to...";
+            // 
+            // metroTile4
+            // 
+            this.metroTile4.BackColor = System.Drawing.Color.White;
+            this.metroTile4.ForeColor = System.Drawing.Color.Transparent;
+            this.metroTile4.Location = new System.Drawing.Point(50, 102);
+            this.metroTile4.Name = "metroTile4";
+            this.metroTile4.Size = new System.Drawing.Size(196, 102);
+            this.metroTile4.Style = MetroFramework.MetroColorStyle.Orange;
+            this.metroTile4.TabIndex = 4;
+            this.metroTile4.Text = "Join a quiz";
+            this.metroTile4.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroTile4.TileImage = global::KalutClient.Properties.Resources.rsz_1rsz_plus_512;
+            this.metroTile4.TileImageAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.metroTile4.UseTileImage = true;
+            // 
+            // metroTile2
+            // 
+            this.metroTile2.Location = new System.Drawing.Point(151, 210);
+            this.metroTile2.Name = "metroTile2";
+            this.metroTile2.Size = new System.Drawing.Size(95, 58);
+            this.metroTile2.TabIndex = 2;
+            this.metroTile2.Text = "Create a quiz";
+            this.metroTile2.TileImage = global::KalutClient.Properties.Resources.rsz_notepad;
+            this.metroTile2.TileImageAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.metroTile2.UseTileImage = true;
+            // 
+            // metroTile1
+            // 
+            this.metroTile1.Location = new System.Drawing.Point(50, 210);
+            this.metroTile1.Name = "metroTile1";
+            this.metroTile1.Size = new System.Drawing.Size(95, 58);
+            this.metroTile1.TabIndex = 0;
+            this.metroTile1.Text = "Host a quiz";
+            this.metroTile1.TileImage = global::KalutClient.Properties.Resources.rsz_player_copy;
+            this.metroTile1.TileImageAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.metroTile1.UseTileImage = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(296, 323);
+            this.Controls.Add(this.metroTile4);
+            this.Controls.Add(this.metroTile2);
+            this.Controls.Add(this.metroLabel1);
+            this.Controls.Add(this.metroTile1);
+            this.ForeColor = System.Drawing.Color.Transparent;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(296, 323);
+            this.MinimumSize = new System.Drawing.Size(296, 323);
+            this.Name = "Form1";
+            this.Text = "Welcome to Kalut!";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private MetroFramework.Controls.MetroTile metroTile1;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroTile metroTile2;
+        private MetroFramework.Controls.MetroTile metroTile4;
     }
 }
 
--- KalutClient/KalutClient/Form1.cs
+++ KalutClient/KalutClient/Form1.cs
@@ -7,14 +7,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MetroFramework.Forms;
 
 namespace KalutClient
 {
-    public partial class Form1 : Form
+    public partial class Form1 : MetroForm
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            
         }
     }
 }
